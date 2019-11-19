89a11c6200518317162e2a6bcb2035f4466d36a4
Steven Bell <thebells1111@gmail.com>
2019-11-18 21:18:34 -0600

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,35 @@
+<script>
+	import * as monaco from 'monaco-editor'
+	import { onMount } from 'svelte'
+
+	let markupEditor = monaco.editor;
+	let html = ''
+
+	onMount(()=>{
+		markupEditor.create(document.getElementById('container'), {
+		value: 'console.log("Hello, world")',
+		language: "markdown",
+		wordWrap: 'wordWrapColumn',
+		wordWrapColumn: 80,	
+		wordWrapMinified: true,
+		wrappingIndent: "indent",
+		lineNumbers: "off",
+		scrollBeyondLastLine: false,
+		});
+	})
+
+	function markup(e){
+		console.log(markupEditor)
+		html = markupEditor.getModels()[0].getValue();
+	}
+
+</script>
+
+<style>
+	
+</style>
+
+<div style="height:95%; display: flex">
+<div id="container" style="height:95%; width: 45%" on:keyup={markup}></div>
+<div style="height:95%; width: 45%">{@html html}</div>
+</div>
