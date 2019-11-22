5d74e124f6fdceaae8804b3bdc9910590b22acf5
thebells1111 <thebells1111@gmail.com>
2019-11-22 14:29:47 -0600

    adding more componentization

--- src/App.svelte
+++ src/App.svelte
@@ -2,42 +2,17 @@
   import { onMount } from "svelte";
   import { saveLoad }from "../util/saveLoad";
   import Editor from "../components/MonacoEditor.svelte";
-  import "../public/prism.css";
-  import * as prism from "markdown-it-prism";
-  import * as markdownItAttrs from "markdown-it-attrs";
-  const md = require("markdown-it")({
-    html: true // Enable HTML tags in source
-  });
-  md.use(markdownItAttrs, {
-    // optional, these are default options
-    leftDelimiter: "{",
-    rightDelimiter: "}",
-    allowedAttributes: [] // empty array = all attributes are allowed
-  });
-  md.use(prism);
-
-  let html=''
+  import Viewer from "../components/MarkdownViewer.svelte";
+  import { markdownOptions, styleOptions } from "../util/editorOptions"
+ 
+  let markdown='';
+  let html ='';
   let markdownModel;
   let styleModel;  
-  let fileName = "markdown";
-
-  const markdownOptions = {
-    language: "markdown",
-    textModelOptions: { tabSize: 2 },
-    editorOptions: {
-      wordWrap: "wordWrapColumn",
-      wordWrapColumn: 60,
-      wordWrapMinified: true,
-      wrappingIndent: "indent"
-    }
-  };
-
-  const styleOptions = {...markdownOptions}
-  styleOptions.language = 'css'
+  let fileName = "markdown";  
 
   function markdownContentChange(e) {
-    let markdown = e.detail.value;
-    html = md.render(markdown);
+    markdown = e.detail.value;
     window.localStorage.setItem("markdown", markdown);
   }
 
@@ -66,17 +41,10 @@
   padding: 1em;
   height: calc(100vh - 3em);
 }
-#editor > div {
-  width: 100%;
-  flex-grow: 1;
-}
 
-#viewer {
-  overflow-y: auto;
-}
 </style>
 
-<svelte:window on:keydown|preventDefault="{handleKeydown}" />
+<svelte:window on:keydown="{handleKeydown}" />
 
 <svelte:head>
   <style type="text/css" id="styler"></style>
@@ -85,8 +53,6 @@
 <div id="editor">  
   <Editor bind:model={styleModel} {...styleOptions} on:didContentChange={styleContentChange}/>
   <Editor bind:model={markdownModel} {...markdownOptions} on:didContentChange={markdownContentChange}/>
-  <div id="viewer">
-    {@html html}
-  </div>
+  <Viewer bind:html {markdown}/>  
 </div>
 
